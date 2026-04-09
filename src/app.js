778c8944f82d0c764ff505d79e5d0cfeb26854f9
Jayan <jayansoni818@gmail.com>
2026-04-09 15:07:01 +0530

    Debugging

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use("/api/v1",schoolRouter);
 
 app.use((req,res) => {
-    console.log(`Invalid Request came at Url : ${req.url} of Type : ${req.method}`);
+    console.log(`Invalid Request came at Url : ${req.url} of Type : ${req.method}, Req : ${req.body}`);
     res.status(404).json({ message:"Resource Not Found" });
 });
 
